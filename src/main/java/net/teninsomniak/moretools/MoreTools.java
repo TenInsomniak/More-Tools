2cbf4b701d8681767644b677ad03e2aa686f0d0e
TenInsomniak <114576919+TenInsomniak@users.noreply.github.com>
2024-09-02 22:00:12 +0200

    Edited the fabric mod template

---
+++ src/main/java/net/teninsomniak/moretools/MoreTools.java
@@ -0,0 +1,14 @@
+package net.teninsomniak.moretools;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreTools implements ModInitializer {
+	public static final String MOD_ID = "moretools";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/teninsomniak/moretools/MoreToolsClient.java
@@ -0,0 +1,11 @@
+package net.teninsomniak.moretools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreToolsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
