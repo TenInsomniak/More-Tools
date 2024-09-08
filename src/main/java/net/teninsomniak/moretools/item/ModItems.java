2267bfbb4cc18e46628aea606f8fb474173f84c5
TenInsomniak <114576919+TenInsomniak@users.noreply.github.com>
2024-09-08 14:50:05 +0200

    Made the weapons fully useable

--- src/main/java/net/teninsomniak/moretools/item/ModItems.java
+++ src/main/java/net/teninsomniak/moretools/item/ModItems.java
@@ -2,6 +2,7 @@ package net.teninsomniak.moretools.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 //import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.tag.ItemTags;
 import net.teninsomniak.moretools.MoreTools;
@@ -11,26 +12,28 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-
     public static final Item WOODEN_SCYTHE = registerItem("wooden_scythe",
-            new Item(new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+            new ScytheItem(ToolMaterials.WOOD, 4, -3.2f, new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+
     public static final Item STONE_SCYTHE = registerItem("stone_scythe",
-            new Item(new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+            new ScytheItem(ToolMaterials.STONE, 4, -3.2f, new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+
     public static final Item IRON_SCYTHE = registerItem("iron_scythe",
-            new Item(new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+            new ScytheItem(ToolMaterials.IRON, 4, -3.1f, new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+
     public static final Item GOLD_SCYTHE = registerItem("gold_scythe",
-            new Item(new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+            new ScytheItem(ToolMaterials.GOLD, 4, -3f, new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+
     public static final Item DIAMOND_SCYTHE = registerItem("diamond_scythe",
-            new Item(new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+            new ScytheItem(ToolMaterials.DIAMOND, 4, -3f, new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+
     public static final Item NETHERITE_SCYTHE = registerItem("netherite_scythe",
-            new Item(new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+            new ScytheItem(ToolMaterials.NETHERITE, 4, -3f, new FabricItemSettings().group(ModItemGroup.SCYTHES)));
+
 
 
     public static final Item TEST_ITEM = registerItem("test_item",
-            new ScytheItem(ToolMaterials.WOOD),
-            ToolMaterials.WOOD.getAttackDamage(),
-            ToolMaterials.WOOD.getMiningSpeedMultiplier(),
-            ItemTags.PLANKS);
+            new ScytheItem(ToolMaterials.WOOD, 2, -2.8f, new FabricItemSettings().group(ModItemGroup.SCYTHES)));
 
 
 
--- src/main/java/net/teninsomniak/moretools/item/ScytheItem.java
+++ src/main/java/net/teninsomniak/moretools/item/ScytheItem.java
@@ -1,30 +1,11 @@
 package net.teninsomniak.moretools.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.*;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
 
 public class ScytheItem extends SwordItem {
-    String[] repairIngredient;
-
-    public ScytheItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, String... repairIngredient) {
-        super(toolMaterial, attackDamage, attackSpeed,
-                new FabricItemSettings().group(ModItemGroup.SCYTHES));
-        this.repairIngredient = repairIngredient;
-    }
-
-    @Override
-    public boolean canRepair(ItemStack stack, ItemStack ingredient) {
-        List<Item> potentialIngredients = new ArrayList<>(List.of());
-        Arrays.stream(repairIngredient).toList().forEach(repIngredient ->
-                potentialIngredients.add(
-                        Registry.ITEM.get(new Identifier(repIngredient))));
-
-
-        return potentialIngredients.contains(ingredient.getItem());
+    public ScytheItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Item.Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
     }
 }
